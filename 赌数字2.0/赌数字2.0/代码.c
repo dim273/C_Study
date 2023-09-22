7c19a16d94d2e474e81e0eef88874c13599f1326
birddim <18778367288@163.com>
2023-09-22 14:00:18 +0800

    用switch写的猜数字游戏

---
+++ 赌数字2.0/赌数字2.0/代码.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+int main()
+{
+	srand(time(NULL));
+	int a, b, sum;
+	printf("请输入一个一到十以内的数字！\n");
+	scanf("%d", &a);
+	b = rand() % 10;
+	sum = a + b;
+	switch (sum)
+	{
+	case 3:
+		printf("和为%d,是一等奖", sum);
+		break;
+	case 9:
+		printf("和为%d,是二等奖", sum);
+		break;
+	case 12:
+		printf("和为%d,是二等奖", sum);
+		break;
+	case 18:
+		printf("和为%d,是三等奖", sum);
+		break;
+	case 6:
+		printf("和为%d,是三等奖", sum);
+		break;
+	case 1:
+		printf("和为%d,是三等奖", sum);
+		break;
+	default:
+		printf("和为%d，很遗憾，你没中奖。", sum);
+		break;
+	}
+	return 0;
+}
