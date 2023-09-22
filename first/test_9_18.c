81b94a43f6f3b64ac289fed1eec531a60c3a38c3
玲子 <18778367288@163.com>
2023-09-22 06:34:03 +0000

    update first/test_9_18.c.
    Signed-off-by: 玲子 <18778367288@163.com>

--- first/test_9_18.c
+++ first/test_9_18.c
@@ -1,38 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#include<time.h>
-#include<stdlib.h>
 int main()
 {
-	srand(time(NULL));
-	int a,b,sum;
-	printf("请输入一个一到十以内的数字！\n");
-	scanf("%d", &a);
-	b = rand() % 10;
-	sum = a + b;
-	switch (sum)
-	{
-	case 3:
-		printf("和为%d,是一等奖", sum);
-		break;
-	case 9:
-		printf("和为%d,是二等奖", sum);
-		break;
-	case 12:
-		printf("和为%d,是二等奖", sum);
-		break;
-	case 18:
-		printf("和为%d,是三等奖", sum);
-		break;
-	case 6:
-		printf("和为%d,是三等奖", sum);
-		break;
-	case 1:
-		printf("和为%d,是三等奖", sum);
-		break;
-	default:
-		printf("和为%d，很遗憾，你没中奖。", sum);
-		break;
-	}
+	struct Stu s={"birddim",19,"boy","play game"};
+    printf("%s %d %s %s",s.name,s.age,s.sex,s.hobby);
 	return 0;
+}
+struct Stu
+{
+    char name[20];
+    int age;
+    char sex;
+    char hobby [30];
 }
