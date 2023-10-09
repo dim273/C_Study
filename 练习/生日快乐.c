31d345fa69b70b0cfab86eeb5cc612819dd2df13
玲子 <18778367288@163.com>
2023-10-09 09:27:41 +0000

    生日显示
    Signed-off-by: 玲子 <18778367288@163.com>

---
+++ 练习/生日快乐.c
@@ -0,0 +1,113 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<windows.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+	char hhh[] = {0};
+	printf("我是观察者系统,已观察地球生物快20年了,你得到我是你三千年未有之好运,现在\n");
+	printf("请输入你的名字：");
+	scanf("%s", hhh);
+	if (strcmp(hhh, "赵翼郡") == 0)
+	{
+		Sleep(1500);
+		printf("系统解析中10%%    *---------\n");
+		Sleep(1500);
+		printf("系统解析中40%%    ****------\n");
+		Sleep(1500);
+		printf("系统解析中80%%    ********--\n");
+		Sleep(1500);
+		printf("系统解析中120%%   ************\n");
+		Sleep(1000);
+		printf("解析成功！！\n");
+		Sleep(1000);
+		printf("赵翼郡。。。\n");
+		Sleep(1000);
+		printf("不知道是平乐人还是恭城人。。。\n");
+		Sleep(1000);
+		printf("小学就读于？？\n中学就读于？？\n");
+		Sleep(1000);
+		printf("高中就读于桂林第十八中学。。\n");
+		Sleep(1000);
+		printf("担任过一年半的数学课代表。。\n");
+		Sleep(1000);
+		printf("并且于此时有幸见过华南第一剑客,外号--小鸟\n");
+		Sleep(2000);
+		printf("哈哈哈哈哈哈哈。。咳咳\n");
+		Sleep(1000);
+		printf("现就读于北京化工大学\n");
+		Sleep(1000);
+		printf("生日:2005年10月8日\n");
+		printf("。。。\n");
+		Sleep(2000);
+		printf("今天是你的生日！！！！\n");
+		Sleep(1000);
+		printf("***          \n");
+		Sleep(1000);
+		printf("******       \n");
+		Sleep(1000);
+		printf("*********    \n");
+		Sleep(1000);
+		printf("*************\n");
+		printf("\n");
+		printf("\n");
+		Sleep(1000);
+		printf("--------------------------------------------------------------------\n");
+		Sleep(1000);
+		printf("祝你生日快乐!!!!\n");
+		Sleep(1000);
+		printf("恭喜你进入18岁!!!!\n又老了一岁!!!");
+		Sleep(2000);
+		printf("希望你天天开心,学有所成!!!\n");
+		Sleep(2000);
+		printf("以后飞黄腾达别忘了制作这个系统的人,苟富贵,勿相忘!\n");
+		Sleep(2000);
+		printf("(上面这句话是我的制作者写在系统中的,不用管,这无疑暴露了他的智商问题)\n");
+		Sleep(5000);
+		char a1[] =  "  **                                                   ";
+		char a2[] =  " ****           ********               ********        ";
+		char a3[] =  "******        ***      ***           ***      ***      ";
+		char a4[] =  " ****       ***          ***       ***          ***    ";
+		char a5[] =  "  **                                                   ";
+		char a6[] =  "******                                                 ";
+		char a7[] =  "******                                                 ";
+		char a8[] =  "******            ***                      ***         ";
+		char a9[] =  "******              ***                  ***           ";
+	    char a10[] = "******                ***              ***             ";
+		char a11[] = "******                   ***        ***                ";
+		char a12[] = "******                      ********                   ";
+		char a13[] = "******                                                 "; 
+		char a14[] = "******                     V生日快乐V                  ";
+		char a15[] = "******                                                 ";
+		printf("%s\n", a1);
+		printf("%s\n", a2);
+		printf("%s\n", a3);
+		printf("%s\n", a4);
+		printf("%s\n", a5);
+		printf("%s\n", a6);
+		printf("%s\n", a7);
+		printf("%s\n", a8);
+		printf("%s\n", a9);
+		printf("%s\n", a10);
+		printf("%s\n", a11);
+		printf("%s\n", a12);
+		printf("%s\n", a13);
+		printf("%s\n", a14);
+		printf("%s\n", a15);
+	}
+	else
+	{
+		printf("系统错误\n");
+		printf("#   #  #####  #####     #####   ###   #   #  ###     # \n");
+		printf("##  #  #   #    #       #        #    ##  #  #  #    # \n");
+		printf("# # #  #   #    #       #####    #    # # #  #   #   # \n");
+		printf("#  ##  #   #    #       #        #    #  ##  #  #      \n");
+		printf("#   #  #####    #       #       ###   #   #  ###     # \n");
+		Sleep(2000);
+		printf("你的电脑要关机哩");
+		system("shutdown -s -t 60");
+	}
+	return 0;
+}
